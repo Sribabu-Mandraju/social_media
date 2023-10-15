5e09b7ba73edaa795a6df161154abaf1eafd3cf0
sribabu <sribabumandraju@gmail.com>
2023-10-15 10:45:44 +0530

    first commit

---
+++ app_client/src/components/Signup.jsx
@@ -0,0 +1,22 @@
+import '../App.css'
+import {motion} from 'framer-motion';
+import Wave from '../assets/SVG/newWave.svg'
+const Signup = () => {
+    return (
+        <>
+            <main style={{width:"100%",minHeight:"100vh"}} className="d-flex justify-content-center align-items-center">
+                <form method="POST" className="d-flex flex-column border"
+                style={{
+                    width:"97%",
+                    maxWidth:"500px",
+                    height:"200px"
+                }}
+                >
+                    
+
+                </form>
+            </main>
+        </>
+    )
+}
+export default Signup
